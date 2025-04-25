f003c4dbf8ae0226b266933a64d869abbee5f48e
Sergio-P23 <patarroyoarias23@gmail.com>
2025-04-25 16:57:48 -0500

    creacion de proyecto con spring initializr para la implementacion del mvc

---
+++ R2-A3-S10 Construyendo TDA/cliente/src/test/java/com/listadoble/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listadoble.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
