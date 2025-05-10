93ee67fe0eb894b9bc29dfde9684f464221af13a
Sergio-P23 <patarroyoarias23@gmail.com>
2025-05-10 11:16:17 -0500

    creacion de proyecto lista circular

---
+++ R3-A1-S12 Lista Circular/src/src/test/java/com/lista_circular/src/SrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lista_circular.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
